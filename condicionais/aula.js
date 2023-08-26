39b2987d4b925edfb3c4f1dcbd79bb4240437a6f
reisfellipe <125813498+reisfellipe@users.noreply.github.com>
2023-08-25 23:21:19 -0300

    praticing

--- condicionais/aula.js
+++ condicionais/aula.js
@@ -2,6 +2,22 @@ const camisetaFellipeAzul = true;
 const camisetaReisAzul = false;
 
 //teste de impar ou par
-const numero = 10;
+const numero = 0;
 const numeroPar = (numero % 2) === 0;
-console.log(numeroPar);
+
+if(numeroPar){
+    console.log('Par')
+}else {
+    console.log('impar')
+}
+
+//segunda forma mais trabalhada
+
+const num = 9;
+const numDivisivelPor5 = (num % 5) === 0;
+
+if(numDivisivelPor5) {
+    console.log(`o numero ${num} é DIVISÍVEL`);
+}else{
+    console.log(`O numero ${num} NÃO É DIVISÍVEL`);
+}
---
+++ condicionais/desafio.js
@@ -0,0 +1,24 @@
+const custoEtanol = 3.61;
+const custoGasolina = 5.82;
+const myFuels = "Etanol";
+const distanceKM = 25;
+const gastoMedio = 0.43;
+
+if(myFuels == 'Etanol' || myFuels == 'etanol'){
+    totalViagem = custoEtanol / distanceKM
+    console.log(`Sua viagem custará R$${totalViagem}`);
+}else if (myFuels == 'Gasolina' || myFuels == 'gasolina'){
+    totalViagem = custoGasolina / distanceKM
+    console.log(`A viagem custará R$${totalViagem}`);
+}else{
+    console.log("Verifique os valores, e preencha corretamente.")
+}
+
+//solução feita pelo alemaozin
+const precoCombustivel = 5.79;
+const KmPorLitros = 10;
+const distanciaEmKm = 100;
+const litrosConsumidos = distanciaEmKm / KmPorLitros;
+const valorGasto = litrosConsumidos * precoCombustivel;
+
+console.log(valorGasto.toFixed(2));
