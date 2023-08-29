f9c14e3cfcf2cd722791fd1c7700d914a2b4976f
reisfellipe <125813498+reisfellipe@users.noreply.github.com>
2023-08-29 17:20:57 -0300

    functions js

---
+++ funcoes/ex03.js
@@ -0,0 +1,28 @@
+//exemplo de funções eficientes para calcular IMC
+
+function calcularImc(peso, altura){
+    return peso / Math.pow(altura,2);
+}
+function classificarImc(imc){
+    if(imc < 18.5){
+        return "Abaixo do peso";
+    }else if(imc > 18.5 && imc < 25){
+        return "Peso normal";
+    }else if(imc >= 25 && imc < 30){
+        return "Acima do peso";
+    }else if(imc >= 30 && imc < 40){
+        return "Obseidade";
+    }else if(imc >= 40){
+        return 'Obesidade grave';
+    }
+}
+//PARA USAR
+function main(){
+    const peso = 58;
+    const altura = 1.69;
+    const imc = calcularImc(peso,altura);
+
+    console.log(classificarImc(imc));
+}
+//chamando a função
+main();
