9a0341070d7aadd7f395e5c2d76830443838d510
reisfellipe <125813498+reisfellipe@users.noreply.github.com>
2023-08-30 00:01:33 -0300

    update-ex-04

--- listaexercicio/ex-04.js
+++ listaexercicio/ex-04.js
@@ -16,15 +16,22 @@ verificarIdade(18);
 function aplicarDesconto(valor,desconto){
     return (valor - (valor * desconto / 100));
 }
+
+function aplicarJuros(valor,juros){
+    return (valor + (valor * (juros / 100)));
+}
+
 const precoEtiqueta = 100;
-const formaDePagamento = 1;
+const formaDePagamento = 4;
 
 if(formaDePagamento === 1){
     console.log(aplicarDesconto(precoEtiqueta, 10));
 }else if(formaDePagamento === 2){
-    console.log(precoEtiqueta - (precoEtiqueta * 0.15));
+    console.log(aplicarDesconto(precoEtiqueta, 15));
 }else if(formaDePagamento === 3){
     console.log(precoEtiqueta + " Sem juros");
 }else{
-    console.log(precoEtiqueta + (precoEtiqueta * 0.1));
+    console.log(aplicarJuros(precoEtiqueta, 10));
 }
+console.log(200 * 0.10);
+console.log(10 / 100);
