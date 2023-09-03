0b3034b42722fb6bbc40c053a89c803be7da1693
reisfellipe <125813498+reisfellipe@users.noreply.github.com>
2023-09-03 00:38:26 -0300

    learning arrays

--- Arrays/aula-02.js
+++ Arrays/aula-02.js
@@ -1,7 +1,11 @@
 const animais = ['Cachoro', 'Gato', 'Leão'];
 
-animais.push('Gavião'); //PUSH adiciona um item
+animais.push('Gavião'); //PUSH adiciona/substitui um item
 
 animais[4] = 'Zebra';
 
 console.log(animais);
+
+console.log(animais.pop());  //exclui o último item
+
+console.log(animais.shift()); //exclui o primeiro Item
---
+++ Arrays/aula-03.js
@@ -0,0 +1,16 @@
+const notas = [];
+
+notas.push(5);
+notas.push(7);
+notas.push(8);
+notas.push(2);
+notas.push(5);
+
+const soma = notas[0] + notas[1] + notas[2] + notas[3] + notas[4]
+
+//  ITERAÇÃO PARA REPETIÇÃO
+for(let i = 0; i < 10; i++){
+    console.log(i);
+}
+
+console.log(soma / notas.length)  //Mostra quantos itens tem na lista
