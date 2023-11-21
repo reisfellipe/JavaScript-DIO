b4341611588a93efa822be78922ebd9b1554e155
Fellipe <reisfellipe11@gmail.com>
2023-11-21 17:17:13 -0300

    add another exercise

--- MDN-JS/ex-01.js
+++ MDN-JS/ex-01.js
@@ -2,7 +2,10 @@ var shopping = ["bread", "milk", "cheese", "hummus", "noodles"]
 var shop = shopping.map((product)=>{
     return product.replace("bread", "CHANGED")
 })
-console.log(shop)
+
+for(let i = 0; i < shopping.length; i++){
+    console.log(shop[i])
+}
 
 
 var sequence = [1, 1, 2, 3, 5, 8, 13];
@@ -10,10 +13,10 @@ var random = ["tree", 795, [0, 1, 2]];
 
 const myArray = ["Auto ajuda", "Psicologia", 19, "inglês", 2004];
 
-console.log(myArray[1])
+// console.log(myArray[1])
 
 myArray[1] = "Pizza de chocolate";
-console.log(myArray[1])
+// console.log(myArray[1])
 
 myArray.push(sequence[sequence.length - 1])
-console.log(myArray[1][1])
+// console.log(myArray[1][1])
